e888f0b3b487d6a4dc0f2990ed3059331de31b57
Léo Rolland <rolland.leo@orange.fr>
2020-08-07 23:44:26 +0000

    ADD account creation test

--- cypress/integration/account.js
+++ cypress/integration/account.js
@@ -1,5 +1,25 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
+describe('Account creation', () => {
+    it('Create an account', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8080/mynotes/account?email=test%40example.com',
+            failOnStatusCode: false
+        })
+        .then((res) => {
+            expect(res.status).to.eq(200)
+            expect(res.body).to.have.property('accountUuid')
+            expect(res.body).to.have.property('email')
+            expect(res.body.email).to.eq('test@example.com')
+        })
+    })
+    it('Create an already existing account', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8080/mynotes/account?email=test%40example.com',
+            failOnStatusCode: false
+        })
+        .then((res) => {
+            expect(res.status).to.eq(403)
+        })
     })
   })
