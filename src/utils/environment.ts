040d0010400ad832f055997e28f43492268c8059
Léo Rolland <rolland.leo@orange.fr>
2020-08-08 15:41:01 +0000

    ADD hot code reload & env as JSON

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,9 +1,13 @@
+const path = require('path')
+const fs = require('fs')
 console.log(`Using ${process.argv[2]} environment.`)
+
+let envPath = path.join(__dirname, `../../environment/environment.${process.argv[2]}.json`)
 let env;
 try {
-    env = require(`../../environment/environment.${process.argv[2]}`)
+    env = JSON.parse(fs.readFileSync(envPath))
 } catch {
-    throw new Error(`Cannot find '${process.argv[2]}' environment. Please create a 'environment/environment.${process.argv[2]}.ts' file.`)
+    throw new Error(`Cannot find '${envPath}' environment. Please create a 'environment/environment.${process.argv[2]}.ts' file.`)
 }
 
 export = env
