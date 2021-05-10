b2c71d8059a6497353552a9e7d09b4923181e7f1
Lorand Valentin <valentin.lorand@etudiant.univ-rennes1.fr>
2021-05-10 14:12:12 +0200

    Improve environment vars

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,15 +1,27 @@
 // const path = require('path')
 // const fs = require('fs')
-// console.log(`Using ${process.argv[2]} environment.`)
+console.log(`Using ${process.argv[2]} environment.`)
 
+let env;
 // let envPath = path.join(__dirname, `../../environment/environment.${process.argv[2]}.json`)
-// let env;
 // try {
 //     env = JSON.parse(fs.readFileSync(envPath))
 // } catch {
 //     throw new Error(`Cannot find '${envPath}' environment. Please create a 'environment/environment.${process.argv[2]}.ts' file.`)
 // }
 
-// export = env
+
+if (process.argv[2] == "prod") {
+    env = {
+        "api":{
+            "port": 8080,
+            "cleanDbBeforeRun":false,
+            "enableTestMode":false
+        }
+    }
+    
+}
+
+export = env
 
 //A supprimer à terme VLO
