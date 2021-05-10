8ca1c3c765cea08e048dc07688d550961d3d8bf2
Lorand Valentin <valentin.lorand@etudiant.univ-rennes1.fr>
2021-05-10 11:36:06 +0200

    Start improve config with docker-compose env

--- src/orm/db.ts
+++ src/orm/db.ts
@@ -2,10 +2,14 @@ import { Sequelize } from 'sequelize-typescript';
 import { Account } from './models/Account';
 import { NoteMetadata } from './models/NoteMetadata';
 import { NoteContent } from './models/NoteContent';
-const env = require('../utils/environment')
 
 export const sequelize = new Sequelize({
-    ...env.db,
+        "dialect":"postgres",
+        "host":"intranet",
+        "port":5432,
+        "username":process.env.POSTGRES_USER,
+        "password":process.env.POSTGRES_PASSWORD,
+        "database":process.env.POSTGRES_DB,
     models: [
         Account,
         NoteMetadata,
